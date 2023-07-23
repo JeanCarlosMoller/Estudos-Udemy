371ed93e1b26bba21a1e314348e8247ea6a96033
Jean Carlos Möller <jeancmoller17@gmail.com>
2023-07-23 18:03:14 -0300

    Dando continuidade aos estudos

---
+++ JavaScript/2_Projetos/8_boolean_op_logico/scripts.js
@@ -0,0 +1,10 @@
+console.log(5 > 3 && 3 == 2);
+console.log(5 > 3 || 3 == 1);
+
+console.log(3 == 3 && "Möller" == "Möller");
+console.log("Jean" == "Carlos" || false);
+
+console.log(!(!(true && true)));
+
+console.log(true && true);
+console.log(false || false);
---
+++ JavaScript/2_Projetos/9_boolean_op_ternario/scripts.js
@@ -0,0 +1,3 @@
+console.log(5 > 2 ? 'É sim' : 'É não');
+console.log(false ? 5 : 4);
+console.log("Jean" == "Jean" ? "Olá, Jean!" : "Não é o Jean!");
