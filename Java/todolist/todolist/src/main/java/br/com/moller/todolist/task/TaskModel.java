ad8ecba60651594a59bfe5e9545e9935ec23c123
Jean Carlos Möller <jeancmoller17@gmail.com>
2023-10-16 22:16:36 -0300

    Finalizando projeto - Dockerfile deploy

--- Java/todolist/todolist/src/main/java/br/com/moller/todolist/task/TaskModel.java
+++ Java/todolist/todolist/src/main/java/br/com/moller/todolist/task/TaskModel.java
@@ -34,7 +34,6 @@ public class TaskModel {
 
     @Column(length = 50)
     private String title;
-
     private LocalDateTime startAt;
     private LocalDateTime endAt;
     private String priority;
@@ -43,4 +42,12 @@ public class TaskModel {
 
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    public void setTitle(String title) throws Exception {
+        if (title.length() > 50) {
+            throw new Exception("O campo title deve conter no máximo 50 caracteres");
+        }
+        this.title = title;
+    }
+
 }
